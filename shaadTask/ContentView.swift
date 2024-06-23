c8630e7bf3c263f512ec522226ddf2180e0310be
ritik-karma <122218104+ritik-karma@users.noreply.github.com>
2024-06-23 10:33:39 +0530

    initial comit

--- shaadTask/ContentView.swift
+++ shaadTask/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("Find you")
+                Text("first")
+                    .foregroundStyle(.red)
+            }
+            HStack{
+                Text("Meta")
+                    .foregroundStyle(.red)
+                Text("Matches")
+            }
         }
-        .padding()
+        .font(.title)
     }
 }
 
