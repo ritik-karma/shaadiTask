3ceb6d0bfb8cf6dd81abfb6ec922977156c0448a
ritik-karma <itsritikkarma@gmail.com>
2024-06-23 14:03:14 +0530

    progress

---
+++ shaadTask/View/loginView.swift
@@ -0,0 +1,18 @@
+//
+//  loginView.swift
+//  shaadTask
+//
+//  Created by Ritik Karma on 23/06/24.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginView()
+}
---
+++ shaadTask/mainView.swift
@@ -0,0 +1,126 @@
+//
+//  mainView.swift
+//  shaadTask
+//
+//  Created by Ritik Karma on 23/06/24.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    var body: some View {
+        NavigationStack{
+            GeometryReader{ geometry in
+                
+                VStack {
+                    VStack{
+                        TabView{
+                            ForEach(0...3, id: \.self) { _ in
+                                Color(.gray)
+                                
+                            }
+                        }
+                        
+                        .frame(width: geometry.size.width, height: geometry.size.height / 2)
+                        .tabViewStyle(.page)
+                        
+                    }
+                    
+                    
+                    
+                    VStack {
+                        
+                        VStack{
+                            HStack{
+                                Text("Find you")
+                                Text("first")
+                                    .foregroundStyle(.red)
+                            }
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            HStack{
+                                Text("Meta")
+                                    .foregroundStyle(.red)
+                                Text("Matches")
+                            }
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            Text("join us and socialize with millions of meta humans")
+                                .font(.subheadline)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                            
+                            VStack(spacing: 25){
+                                HStack(spacing: 10){
+                                    Button{
+                                        //action for action button
+                                    } label: {
+                                        Text("Get Started \(Image(systemName: "arrow.up.right")) ")
+                                            .font(.headline)
+                                            .frame(width: 210, height: 60)
+                                            .background(.red)
+                                            .foregroundColor(.white)
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .cornerRadius(20)
+                                    }
+                                    Button{
+                                        
+                                    } label: {
+                                        Image("google")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 20, height: 20)
+                                            .padding()
+                                            .background(Color.gray.opacity(0.1))
+                                            .cornerRadius(15)
+                                    }
+                                    
+                                    
+                                    Button{
+                                        
+                                    } label: {
+                                        Image("apple")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 20, height: 20)
+                                            .padding()
+                                            .background(Color.gray.opacity(0.1))
+                                            .cornerRadius(15)
+                                    }
+                                    
+                                    }
+                               
+                                HStack {
+                                    Text("Already have an account? ")
+                                        .font(.subheadline)
+                                        .foregroundColor(Color(red: 0, green: 0, blue: 0).opacity(0.50))
+                                    NavigationLink {
+                                        //MARK: Link to login view
+                                    } label: {
+                                        Text("Log In")
+                                            .font(.subheadline)
+                                            .foregroundColor(Color(red: 0.94, green: 0.29, blue: 0.29))
+                                    }
+                                 
+                                }
+                            }
+                            .padding()
+                        }
+                        
+                        
+                    }
+                    .padding()
+                    .frame(width: geometry.size.width, height: geometry.size.height / 2)
+                    
+                }
+            }
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    mainView()
+}
