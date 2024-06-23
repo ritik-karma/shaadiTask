fda968568bb7fb901148811de2fac5d5856f00b1
ritik-karma <122218104+ritik-karma@users.noreply.github.com>
2024-06-23 10:26:49 +0530

    Initial Commit

---
+++ shaadTask/shaadTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaadTaskApp.swift
+//  shaadTask
+//
+//  Created by Ritik Karma on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct shaadTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
